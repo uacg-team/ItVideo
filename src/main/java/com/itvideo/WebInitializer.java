976fd9745333211b2fab00d7363efe49fe402511
velichko <vel.angelov@gmail.com>
2017-10-25 11:25:29 +0300

    init

---
+++ src/main/java/com/itvideo/WebInitializer.java
@@ -0,0 +1,22 @@
+package com.itvideo;
+
+import org.springframework.web.servlet.support.
+AbstractAnnotationConfigDispatcherServletInitializer;
+ 
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { SpringWebConfig.class };
+    }
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/", "*.html", "*.pdf" };
+    }
+}
---
+++ src/main/java/com/itvideo/controlers/MyController.java
@@ -0,0 +1,31 @@
+package com.itvideo.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itvideo.model.Product;
+
+
+@Controller
+public class MyController {
+	
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String sayHi(Model viewModel) {
+		Product product = new Product("domati",5);
+		viewModel.addAttribute("Text","Hello");
+		viewModel.addAttribute("product",product);
+		
+		return "home";
+	}
+	@RequestMapping(value="/homea", method=RequestMethod.GET)
+	public String sayHai(Model viewModel) {
+		Product product = new Product("domati",5);
+		viewModel.addAttribute("Text","Hello");
+		viewModel.addAttribute("product",product);
+		
+		return "home";
+	}
+	
+}
