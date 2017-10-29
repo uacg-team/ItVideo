feb1aa5167149addadbb22ea9fe7e303bbd83644
velichko <vel.angelov@gmail.com>
2017-10-29 12:12:50 +0200

    show more comments

---
+++ src/main/webapp/static/js/test.js
@@ -0,0 +1,4 @@
+function testHtml(){
+	var videoComments = document.getElementById("videoComments");
+	videoComments.innerHTML =buildComment(1,"ala bala",6,23,0,null,false,12,3,"vilio","alabala",1,6);
+}
