a22870e47902dc963d00ec2c5e58cf9874704cf6
velichko <vel.angelov@gmail.com>
2017-11-03 17:29:28 +0200

    CommentService refactor-I

--- src/main/java/com/itvideo/controllers/CommentsService.java
+++ src/main/java/com/itvideo/controllers/CommentsService.java
@@ -20,136 +20,55 @@ import com.itvideo.model.Comment;
 import com.itvideo.model.CommentDao;
 import com.itvideo.model.exceptions.comments.CommentException;
 import com.itvideo.model.exceptions.user.UserException;
-import com.itvideo.model.exceptions.video.VideoException;
 
 @Controller
 @RestController
 public class CommentsService {
 	@Autowired
 	CommentDao comment;
-	
-	@Deprecated
-	@RequestMapping(value = "/comment/video{videoId}", method = RequestMethod.GET)
-	public List<Comment> getCommentsForVideo(@PathVariable long videoId) {
-		List<Comment> comments = null;
-		try {
-			comments = comment.getAllComments(videoId, false);
-			for (Comment c : comments) {
-				List<Comment> replies = comment.getAllReplies(c.getCommentId());
-				c.addReplies(replies);
-				// load likes dislikes for comment:
-				c.setLikes(comment.getLikes(c.getCommentId()));
-				c.setDislikes(comment.getDislikes(c.getCommentId()));
-				// load user info for comment:
-				comment.loadUserInfo(c);
-				// load likes dislikes for reply, and user info:
-				for (Comment reply : replies) {
-					reply.setLikes(comment.getLikes(reply.getCommentId()));
-					reply.setDislikes(comment.getDislikes(reply.getCommentId()));
-					comment.loadUserInfo(reply);
-				}
-			}
-			return comments;
-		} catch (VideoException e) {
-			// TODO
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO
-			e.printStackTrace();
-		} catch (CommentException e) {
-			// TODO
-			e.printStackTrace();
+	//Move to POJO
+	/**
+	 * @param compare-String coming from jsp
+	 * @return comparator for comments, default comparator is <b>DESC_BY_DATE<b>
+	 */
+	private Comparator<Comment> getComparator(String compare){
+		if(compare == null ||compare.isEmpty()) {
+			return CommentDao.DESC_BY_DATE;
 		}
-		return null;
-	}
-	@Deprecated
-	@RequestMapping(value = "/comment/user{userId}", method = RequestMethod.GET)
-	public List<Comment> getCommentsForUser(@PathVariable long userId) {
-		List<Comment> comments = null;
-		try {
-			comments = comment.getAllCommentsByUser(userId, false);
-			for (Comment c : comments) {
-				List<Comment> replies = comment.getAllReplies(c.getCommentId());
-				c.addReplies(replies);
-				// load likes dislikes for comment:
-				c.setLikes(comment.getLikes(c.getCommentId()));
-				c.setDislikes(comment.getDislikes(c.getCommentId()));
-				// load user info for comment:
-				comment.loadUserInfo(c);
-				// load likes dislikes for reply, and user info:
-				for (Comment reply : replies) {
-					reply.setLikes(comment.getLikes(reply.getCommentId()));
-					reply.setDislikes(comment.getDislikes(reply.getCommentId()));
-					comment.loadUserInfo(reply);
-				}
-			}
-			return comments;
-		} catch (SQLException e) {
-			// TODO
-			e.printStackTrace();
-		} catch (CommentException e) {
-			// TODO
-			e.printStackTrace();
-		} catch (UserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		switch (compare) {
+		case "newest":
+			return CommentDao.DESC_BY_DATE;
+		case "oldest":
+			return CommentDao.ASC_BY_DATE;
+		case "mostLiked":
+			return CommentDao.DESC_BY_LIKES;
+		case "mostDisliked":
+			return CommentDao.DESC_BY_DISLIKES;
+		default:
+			return CommentDao.DESC_BY_DATE;
 		}
-		return null;
 	}
-	
+	//refactor js parameters
 	@ResponseBody
 	@RequestMapping(value = "player/getCommentsWithVotes/{videoId}/{myUserId}/{compare}/{part}/{countReplies}", method = RequestMethod.GET)
 	public List<Comment> getCommentsWithVotesForVideo(@PathVariable Long videoId,@PathVariable Long myUserId,@PathVariable String compare,@PathVariable Integer part,@PathVariable Integer countReplies) {
-		System.out.println(compare);
-		if(compare==null) {
-			compare="";
-		}
-		Comparator<Comment> comparator = null;
-		switch (compare) {
-		case "newest":
-			comparator = CommentDao.DESC_BY_DATE;
-			break;
-		case "oldest":
-			comparator = CommentDao.ASC_BY_DATE;
-			break;
-		case "mostLiked":
-			comparator = CommentDao.DESC_BY_LIKES;
-			break;
-		case "mostDisliked":
-			comparator = CommentDao.DESC_BY_DISLIKES;
-			break;
-		default:
-			comparator = CommentDao.DESC_BY_DATE;
-		}
+		Comparator<Comment> comparator=getComparator(compare);
 		List<Comment> comments = null;
 		try {
-			//TODO replace with original getAllCommentsWithVotesByVideo(videoId, myUserId, comparator);
 			comments = comment.getAllCommentWithVotesByVideoWithoutReplies(videoId, myUserId, comparator);
 		} catch (SQLException e) {
 			// TODO handle
+			//response.setStatus(500);
 			e.printStackTrace();
 		}
 		return comments;
 	}
+	
 	//tested
 	@ResponseBody
 	@RequestMapping(value = "player/getRepliesWithVotes/{commentId}/{myUserId}/{compare}", method = RequestMethod.GET)
 	public List<Comment> getRepliesWithVotesForComment(@PathVariable Long commentId,@PathVariable Long myUserId,@PathVariable String compare) {
-		System.out.println(compare);
-		if(compare==null) {
-			compare="";
-		}
-		Comparator<Comment> comparator = null;
-		switch (compare) {
-		case "latest":
-			comparator = CommentDao.ASC_BY_DATE;
-			break;
-		case "oldest":
-			comparator = CommentDao.DESC_BY_DATE;
-			break;
-		default:
-			comparator = CommentDao.DESC_BY_DATE;
-		}
+		Comparator<Comment> comparator=getComparator(compare);
 		List<Comment> comments = null;
 		try {
 			comments = comment.getAllRepliesWithVotesForComment(commentId, myUserId, comparator);
@@ -174,11 +93,13 @@ public class CommentsService {
 			comment.createComment(newComment);
 		} catch (CommentException | SQLException e) {
 			// TODO Auto-generated catch block
+			//TODO js catch errors
 			e.printStackTrace();
 		}
 		System.out.println(newComment.getCommentId());
 		return newComment;
 	}
+	
 	//TODO rename commentLikeTest, like dislike with one function
 	@ResponseBody
 	@RequestMapping(value = "player/commentLikeTest", method = RequestMethod.POST)
@@ -212,6 +133,7 @@ public class CommentsService {
 	@ResponseBody
 	@RequestMapping(value = "player/deleteComment", method = RequestMethod.POST)
 	public void deleteComment(HttpServletRequest req) {
+		//TODO validate again userId
 		long commentId = Long.parseLong(req.getParameter("commentId"));
 		try {
 			comment.deleteComment(commentId);
